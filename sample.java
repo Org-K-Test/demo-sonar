46831ed6031a51743d890180326a9ac279eb3500
KrinaVadgama <86055102+KrinaVadgama@users.noreply.github.com>
2023-05-18 11:23:48 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(sum1(1, 2));
+        System.out.println(sum2(1, 2));
+    }
+    public int sum1(int a, int b){
+        return a + b;
+    }
+    public int sum2(int a, int b){
+        return a + b;
+    }
+}
