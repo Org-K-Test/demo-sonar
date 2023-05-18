2e49b1b267ee741e32a64a5ea3c74825af0461c7
KrinaVadgama <86055102+KrinaVadgama@users.noreply.github.com>
2023-05-18 11:27:08 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,14 +3,18 @@
 class HelloWorld {
     public static void main(String[] args) {
         int a = 1;
-        int b = 2;
-        System.out.println(sum1(1, 2));
-        System.out.println(sum2(1, 2));
+        int b = 0;
+        System.out.println(sum1(a, b));
+        System.out.println(sum2(a, b));
+        System.out.println(div(a, b));
     }
     public int sum1(int a, int b){
         return a + b;
     }
     public int sum2(int a, int b){
         return a + b;
+    }
+    public int div(int a, int b){
+        return a / b;
     }
 }
